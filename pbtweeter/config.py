aecf3ae611cdf999a15f770c1daadbb59ffbe51c
Dutchj <jbndutchj@gmail.com>
2015-07-02 10:31:35 +0200

    Improved config handling
    Previously a default config file was distributed with the bot. Now the bot
    will create a default config file if there is none available.

--- pbtweeter/config.py
+++ pbtweeter/config.py
@@ -1,4 +1,26 @@
 import yaml
+import os
+
+if not os.path.exists('../config/'):
+        os.mkdir('../config/')
+if not os.path.isfile('../config/config.yml'):
+    default = {
+        'Game': 'The Legend of Zelda: Ocarina of Time',
+        'Interval': 60,
+        'Data': '/data/',
+
+        'Categories': {'Any%': '20:00', '100%': '5:00:00'},
+
+        'PB Messages': ['Congratulations to {player} for getting a {time} in {category}!',
+                        '.{player} just got a {time} in {game} {category}!'],
+
+        'WR Messages': ['A new World Record has been set for {category}! {time} by {player}!',
+                        'WR Hype! {player} got a {time} for {game} {category}!'],
+
+        'Tie Messages': ['The world record ({time}) for {category} has been tied by {player}!']
+    }
+    with open('../config/config.yml', 'w+') as f:
+        yaml.safe_dump(default, f, default_flow_style=False)
 
 try:
     with open('../config/config.yml', 'r') as f:
