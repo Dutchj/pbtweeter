3b1ea41a15f591f662a17176d304cde2c7eee78c
Dutchj <jbndutchj@gmail.com>
2015-07-02 10:10:37 +0200

    Made seconds module more robust
    Previously the time functions in seconds.py couldn't handle times shorter
    than an hour very well.
    
    This should be fixed now, with shorter times also getting a prettier
    format string from seconds_to_time.

--- pbtweeter/seconds.py
+++ pbtweeter/seconds.py
@@ -1,11 +1,22 @@
 def time_to_seconds(time):
-    h = int(time[:2])*3600
-    m = int(time[3:5])*60
-    s = int(time[6:8])
+    times = time.split(':')
+    h = 0
+    m = 0
+    s = int(times.pop(-1))
+    if times:
+        m = int(times.pop(-1))*60
+    if times:
+        h = int(times.pop(-1))*3600
     return h+m+s
 
 def seconds_to_time(sec):
     h = divmod(sec,3600)
     m = divmod(h[1],60)
     s = m[1]
-    return "{H}:{M}:{S}".format(H=str(h[0]),M=str(m[0]).zfill(2),S=str(s).zfill(2))
+
+    if sec < 60:
+        return str(sec)+' seconds'
+    elif h[0] == 0:
+        return '{M}:{S}'.format(M=str(m[0]), S=str(s).zfill(2))
+    else:
+        return '{H}:{M}:{S}'.format(H=str(h[0]), M=str(m[0]).zfill(2), S=str(s).zfill(2))
